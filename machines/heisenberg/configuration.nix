f494dcbcfd29c6cefaeb068ea2233d900cb0fcf0
Brice Michallski <brice_michalski@carrefour.com>
2022-10-08 18:30:46 +0200

    grub support efi

--- machines/heisenberg/configuration.nix
+++ machines/heisenberg/configuration.nix
@@ -4,21 +4,14 @@
   networking.hostName = "heisenberg";
 
   boot.loader.grub.enable = true;
-  boot.loader.grub.version = 2;
   boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
   boot.loader.grub.useOSProber = true;
-  boot.loader.systemd-boot.enable = false;
+
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.efi.efiSysMountPoint = "/boot/efi";
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
-
-  # Enable swap on luks
-  boot.initrd.luks.devices."luks-97301938-e0d9-4240-ae53-a767442be510".device = "/dev/disk/by-uuid/97301938-e0d9-4240-ae53-a767442be510";
-  boot.initrd.luks.devices."luks-97301938-e0d9-4240-ae53-a767442be510".keyFile = "/crypto_keyfile.bin";
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
 
   imports = [
     <nixos-hardware/common/pc/ssd>
