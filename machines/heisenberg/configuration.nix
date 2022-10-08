73f7a23da2663e9ec66f16f21683d45832ef8fd6
BriceMichalski <brice.michalski.92@gmail.com>
2022-10-08 15:37:55 +0200

    grub boot loader

--- machines/heisenberg/configuration.nix
+++ machines/heisenberg/configuration.nix
@@ -1,35 +1,24 @@
-{ config, pkgs, ... }:
+{ config, pkgs, environment, ... }:
 
 {
   networking.hostName = "heisenberg";
   
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.useOSProber = true;
+  boot.loader.systemd-boot.enable = false;
   boot.loader.efi.canTouchEfiVariables = true;   
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
-  boot.kernelModules = [ "dell-smm-hwmon" ];
-
-  # Cooling Management
-  services.thermald.enable = true;
-
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
-
-  environment.etc = {
-  # Creates /etc/nanorc
-  nanorc = {
-    text = ''
-      whatever you want to put in the file goes here.
-    '';
-
-    # The UNIX file mode bits
-    mode = "0440";
-  };
-};
 
   # Enable swap on luks
   boot.initrd.luks.devices."luks-97301938-e0d9-4240-ae53-a767442be510".device = "/dev/disk/by-uuid/97301938-e0d9-4240-ae53-a767442be510";
   boot.initrd.luks.devices."luks-97301938-e0d9-4240-ae53-a767442be510".keyFile = "/crypto_keyfile.bin";
+  boot.initrd.secrets = {
+    "/crypto_keyfile.bin" = null;
+  };
+  
+
 }
