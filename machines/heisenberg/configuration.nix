079d0edd5848aec4792860a64974dce7584a4eef
Brice Michallski <brice_michalski@carrefour.com>
2022-10-08 19:16:49 +0200

    right bootloader path

--- machines/heisenberg/configuration.nix
+++ machines/heisenberg/configuration.nix
@@ -9,7 +9,7 @@
   boot.loader.grub.useOSProber = true;
 
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
