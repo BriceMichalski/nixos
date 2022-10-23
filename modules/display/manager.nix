08d3742478c749eca8b0e40394f1eecbef1a0b34
Brice Michalski <brice.michalski.92@gmail.com>
2022-10-23 17:35:00 +0200

    gnome config

--- modules/display/manager.nix
+++ modules/display/manager.nix
@@ -3,8 +3,6 @@
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  services.xserver.desktopManager.gnome.enable = true;
-
   systemd.services."getty@tty1".enable = false;
   systemd.services."autovt@tty1".enable = false;
   services.xserver.displayManager = {
