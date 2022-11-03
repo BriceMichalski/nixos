36233497d325e1d0bcb19cb6619131394496d833
Brice Michallski <brice_michalski@carrefour.com>
2022-11-03 11:03:26 +0100

    feynman

--- machines/feynman/configuration.nix
+++ machines/feynman/configuration.nix
@@ -1,17 +1,21 @@
-{ config, pkgs, ... }:
+{ config, pkgs, environment, ... }:
 
 {
   networking.hostName = "feynman";
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;   
-  boot.loader.efi.efiSysMountPoint = "/boot/efi";
+  boot.loader.grub.enable = true;
+  boot.loader.grub.device = "nodev";
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.useOSProber = true;
 
-  boot.initrd.secrets = {
-    "/crypto_keyfile.bin" = null;
-  };
-  
-  # Enable swap on luks
-  boot.initrd.luks.devices."luks-3fc68794-8efd-4bc3-9f03-b56ab7a604d7".device = "/dev/disk/by-uuid/3fc68794-8efd-4bc3-9f03-b56ab7a604d7";
-  boot.initrd.luks.devices."luks-3fc68794-8efd-4bc3-9f03-b56ab7a604d7".keyFile = "/crypto_keyfile.bin";
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  imports = [
+    <nixos-hardware/common/pc/ssd>
+    <nixos-hardware/common/pc/laptop>
+    <nixos-hardware/common/cpu/amd>
+  ];
 }
